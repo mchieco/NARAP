46eba9a295299d44d93cf1d06d6d13e4f615b898
Mark Russo <mmrusso3@gmail.com>
2019-11-11 17:50:35 -0500

    working on new form still

--- src/views/Form2.vue
+++ src/views/Form2.vue
@@ -1,69 +1,141 @@
 <template>
   <div class="myForm">
-  <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-row class="columnText">
+        <v-col cols="5">
+          <p>NARAP Site</p>
+          <br />
+          <br />
+          <p>RA Initials</p>
+          <br />
+          <br />
+          <p>RA ID</p>
+          <br />
+          <br />
+          <p>Date</p>
+          <br />
+          <p>Time</p>
+                    <br />
+          <p>May I ask how old you</p>
+        </v-col>
+        <v-col cols="6">
+          <v-overflow-btn
+            class="my-2"
+            :items="dropdown_font"
+            label="Choose Site"
+            target="#dropdown-example"
+          ></v-overflow-btn>
+          <v-text-field v-model="name" :counter="3" :rules="nameRules" label="RA Initials" required></v-text-field>
+          <v-text-field
+            v-model="name2"
+            :counter="10"
+            :rules="nameRules"
+            label="RA ID Number"
+            required
+          ></v-text-field>
+          <v-menu
+            ref="menu1"
+            v-model="menu1"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            full-width
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="dateFormatted"
+                label="Date"
+                hint="MM/DD/YYYY format"
+                persistent-hint
+                @blur="date = parseDate(dateFormatted)"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="date" no-title @input="menu1 = false"></v-date-picker>
+          </v-menu>
+          <p>
+            Date in ISO format:
+            <strong>{{ date }}</strong>
+          </p>
+          <v-dialog
+            ref="dialog"
+            v-model="modal2"
+            :return-value.sync="time"
+            persistent
+            width="290px"
+          >
+          <time-modal/>
+            <template v-slot:activator="{ on }">
+              <v-text-field v-model="time" label="Click to select time" readonly v-on="on"></v-text-field>
+            </template>
+            <v-time-picker v-if="modal2" v-model="time" full-width>
+              <v-spacer></v-spacer>
+              <v-btn text color="primary" @click="modal2 = false">Cancel</v-btn>
+              <v-btn text color="primary" @click="$refs.dialog.save(time)">OK</v-btn>
+            </v-time-picker>
+          </v-dialog>
 
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
-      label="Item"
-      required
-    ></v-select>
+          <v-radio-group v-model="ex8" column>
+              <v-radio
+                label="Yes"
+                color="success"
+                value="success"
+              ></v-radio>
+              <v-radio
+                label="Excluded after apporaching"
+                color="warning"
+                value="warning"
+              ></v-radio>
+              <v-radio
+                label="No"
+                color="error"
+                value="error"
+              ></v-radio>
+            </v-radio-group>
+        </v-col>
+      </v-row>
+      
+  <div id="picktime">
+      <time-modal @close="showLogin=false"
+                  :dialog="showLogin"/>
+      </div>
 
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
-      required
-    ></v-checkbox>
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
 
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="validate"
-    >
-      Validate
-    </v-btn>
+      <v-btn color="error" class="mr-4" @click="reset">Reset Form</v-btn>
 
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      Reset Form
-    </v-btn>
-
-    <v-btn
-      color="warning"
-      @click="resetValidation"
-    >
-      Reset Validation
-    </v-btn>
-  </v-form>
+      <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
+    </v-form>
   </div>
+
 </template>
 
 <script>
+import Time from '@/components/timeModal.vue';
+
 export default {
+  components: { 'time-modal': Time },
   data: () => ({
+    dropdown_font: [
+      'Hartford Hospital',
+      'Hackensack UMC',
+      'Kennedu University Hospital',
+      'St. Vincents Meidical Center',
+      'St.Louis University Hospital',
+      'SUNY Upstate',
+      'Pullman Regional',
+      'University of Vermont',
+      'St. Cloud',
+      'UMC of Southern Nevada',
+      'Henry Ford hospital',
+    ],
+    dropdown_icon: [
+      { text: 'list', callback: () => console.log('list') },
+      { text: 'favorite', callback: () => console.log('favorite') },
+      { text: 'delete', callback: () => console.log('delete') },
+    ],
     valid: true,
     name: '',
     nameRules: [
@@ -107,7 +179,7 @@ export default {
   width: 100%;
   height: 1500px;
 }
-.googleForm{
+.googleForm {
   max-width: 100%;
 }
 </style>
