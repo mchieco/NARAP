9bc27bccc5ee676649022d7e70e236b1f8efe302
mchieco <cheeks369@gmail.com>
2019-11-11 17:59:59 -0500

    live data for gender into pie chart

--- src/services/livedata/index.js
+++ src/services/livedata/index.js
@@ -1,1 +1,19 @@
 import axios from 'axios';
+import qs from 'query-string';
+
+async function getData() {
+  const response = await axios.post('https://redcap.vanderbilt.edu/api/',
+    qs.stringify({
+      token: 'A4FD20E1552AD49113B0998FA226EF9E', // gave the values directly for testing
+      content: 'record',
+      format: 'json',
+      fields: 'gender,fake_data_flag,age',
+    }), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+  return response.data;
+}
+
+export default getData;
--- src/views/LiveData.vue
+++ src/views/LiveData.vue
@@ -5,17 +5,21 @@
 </template>
 
 <script>
+import liveDataService from '@/services/livedata';
+
+let females = 0;
+let males = 0;
 export default {
   data() {
     return {
-      series: [45, 55],
+      series: [10, 10],
       chartOptions: {
         labels: ['Male', 'Female'],
         responsive: [{
           breakpoint: 480,
           options: {
             chart: {
-              width: 200,
+              width: 350,
             },
             legend: {
               position: 'bottom',
@@ -24,6 +28,28 @@ export default {
         }],
       },
     };
+  },
+  methods: {
+    async load() {
+      const x = await liveDataService();
+      x.forEach((obj) => {
+        if (obj.gender === '1' && obj.fake_data_flag === '') {
+          males += 1;
+        }
+        if (obj.gender === '2' && obj.fake_data_flag === '') {
+          females += 1;
+        }
+      });
+      console.log(females);
+      console.log(males);
+      this.series = [
+        males,
+        females,
+      ];
+    },
+  },
+  mounted() {
+    this.load();
   },
 
 };
