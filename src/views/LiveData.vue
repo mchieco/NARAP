ad44927673dbe2aedcc38439e48d6d396d60676d
mchieco <cheeks369@gmail.com>
2019-11-12 14:56:20 -0600

    live data first draft

--- src/views/LiveData.vue
+++ src/views/LiveData.vue
@@ -1,6 +1,15 @@
 <template>
-    <div id="chart">
-      <apexchart type=pie width=380 :options="chartOptions" :series="series" />
+    <div class="pa-8">
+      <p>Research Study - Primary Care Tabacco</p>
+      <p>Number of Participants: {{ participants }}</p>
+      <div class="row">
+      <div class="col">
+      <apexchart type=pie width=500 :options="chartOptionsGender" :series="seriesGender" />
+      </div>
+      <div class="col">
+      <apexchart type=bar height=350 width=500 :options="chartOptionsAge" :series="seriesAge"/>
+      </div>
+      </div>
     </div>
 </template>
 
@@ -9,11 +18,18 @@ import liveDataService from '@/services/livedata';
 
 let females = 0;
 let males = 0;
+let age18to22 = 0;
+let age23to25 = 0;
+let age25to30 = 0;
+let age30to40 = 0;
+let age40plus = 0;
+
 export default {
   data() {
     return {
-      series: [10, 10],
-      chartOptions: {
+      participants: 0,
+      seriesGender: [],
+      chartOptionsGender: {
         labels: ['Male', 'Female'],
         responsive: [{
           breakpoint: 480,
@@ -27,25 +43,90 @@ export default {
           },
         }],
       },
+      seriesAge: [{
+        data: [],
+      }],
+      chartOptionsAge: {
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 350,
+            },
+          },
+        }],
+        plotOptions: {
+          bar: {
+            horizontal: false,
+          },
+        },
+        title: {
+          text: 'Participants Vs Age',
+          align: 'center',
+          margin: 10,
+          offsetX: 0,
+          offsetY: 0,
+          floating: false,
+          style: {
+            fontSize: '16px',
+            color: '#263238',
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        xaxis: {
+          categories: ['18-22', '23-25', '25-30', '30-40', '40+',
+          ],
+        },
+      },
     };
   },
   methods: {
     async load() {
       const x = await liveDataService();
       x.forEach((obj) => {
-        if (obj.gender === '1' && obj.fake_data_flag === '') {
-          males += 1;
-        }
-        if (obj.gender === '2' && obj.fake_data_flag === '') {
-          females += 1;
+        if (obj.fake_data_flag === '') {
+          this.participants += 1;
+          if (obj.gender === '1') {
+            males += 1;
+          }
+          if (obj.gender === '2') {
+            females += 1;
+          }
+          if (obj.age >= 18 && obj.age <= 22) {
+            age18to22 += 1;
+          }
+          if (obj.age >= 23 && obj.age <= 25) {
+            age23to25 += 1;
+          }
+          if (obj.age >= 26 && obj.age <= 30) {
+            age25to30 += 1;
+          }
+          if (obj.age >= 31 && obj.age <= 40) {
+            age30to40 += 1;
+          }
+          if (obj.age > 40) {
+            age40plus += 1;
+          }
         }
       });
       console.log(females);
       console.log(males);
-      this.series = [
+      console.log(age30to40);
+      this.seriesGender = [
         males,
         females,
       ];
+      this.seriesAge = [{
+        data: [
+          age18to22,
+          age23to25,
+          age25to30,
+          age30to40,
+          age40plus,
+        ],
+      }];
     },
   },
   mounted() {
