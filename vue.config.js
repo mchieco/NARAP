d4ec2017d4f3d81bd217a1cccc73b123c1502b30
KyleHorton <Kyle.Hortonx6@gmail.com>
2019-11-30 14:42:08 -0500

    three more statics

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
-  devServer:{
-    proxy: {
-      '/': {
-        target: 'http://localhost:5000'
-      }
-    }
-  }
+  // devServer: {
+  //   proxy: {
+  //     '/': {
+  //       target: 'http://localhost:5000',
+  //     },
+  //   },
+  // },
 };
